af811cf8b261a913cf4f630f9505c8037bb5cfed
technoxerve <mdsaney.1986@gmail.com>
2016-03-13 21:38:35 -0400

    New Uploaded Image

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿-- select * from Book
+
+-- select Book_Title, Author, NumberOfCopies from Book
+--select UserName, Name from StudentLogin
+--inner join Student
+--on Student.SId = StudentLogin.SId
+
+--select * From Issue
+
+--select Name, Book_Title, IssueDate from Issue
+--inner join Book
+--on Book.Book_ID = Issue.Book_ID
+--inner join Student
+--on Student.SId = Issue.SId
+
+select Name, NumberOfBooks from StudentLogin
+inner join Student
+on Student.SId = StudentLogin.SId
